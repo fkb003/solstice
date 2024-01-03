4e70224db4f51c86f97da4d4cae5689dac26f9d3
Fahad Khan <139643409+fkb003@users.noreply.github.com>
2024-01-03 10:53:59 +0600

    Create function to add tasks to file

--- src/main.py
+++ src/main.py
@@ -6,3 +6,8 @@ def show_list(filename): # Show tasks in file
             print(f'{index}. {task}', end='') # Print task with index
             index += 1
         print()
+
+def add_task(filename, task): # Add new tasks at bottom in file
+    with open(filename, 'a', encoding='utf-8') as tasklist:
+        # Add newline at end to ensure next added task is on separate line
+        tasklist.write(f'{task}\n')
